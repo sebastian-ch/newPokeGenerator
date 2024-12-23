cc25e76adf816c41cc6d286df7808a99daa7bb8e
Sebastian Hancock <sebastianc.hancock@gmail.com>
2024-12-23 11:11:47 -0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   useEffect(() => {
     if (!pokemons) {
-      //console.log('app')
+      console.log('app')
       getPokemons().then((pokemons: PokemonsType[]) => {
         setPokemons(pokemons.reverse());
       });
