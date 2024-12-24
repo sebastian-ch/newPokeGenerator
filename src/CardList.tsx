5b2aa50a41f6468e711507e439b0f0036e8e6b58
Sebastian H <shancock@innovateteam.com>
2024-12-23 22:24:06 -0800

    add lazy

--- src/CardList.tsx
+++ src/CardList.tsx
@@ -2,34 +2,30 @@ import { useNavigate } from "react-router-dom";
 import { PokemonsType } from "./config";
 
 function CardList({ pokemon }: { pokemon: PokemonsType[] }) {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	function onClick(e: any, po: PokemonsType) {
-		const data = {
-			name: po.name,
-			type: po.type,
-			description: po.description,
-			image: e.target.src,
-		};
-		navigate(`/newPokeGenerator/${po.name}`, { state: data });
-	}
+  function onClick(po: PokemonsType) {
+    const data = {
+      name: po.name,
+      type: po.type,
+      description: po.description,
+      image: po.img,
+    };
+    navigate(`/newPokeGenerator/${po.name}`, { state: data });
+  }
 
-	return (
-		<>
-			{pokemon?.map((po) => (
-				<div
-					key={po.name}
-					className="card"
-					onClick={(e) => onClick(e, po)}
-				>
-					<h4 style={{ color: "whitesmoke", padding: "1px", margin: "2px" }}>
-						{po.name}
-					</h4>
-					<img className="img-list" src={po.img} />
-				</div>
-			))}
-		</>
-	);
+  return (
+    <>
+      {pokemon?.map((po) => (
+        <div key={po.name} className="card" onClick={() => onClick(po)}>
+          <h4 style={{ color: "whitesmoke", padding: "1px", margin: "2px" }}>
+            {po.name}
+          </h4>
+          <img className="img-list" src={po.img} loading="lazy" />
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default CardList;
--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -1,4 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+
+console.log(import.meta.env.VITE_SUPABASE_URL)
+
 const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_KEY
